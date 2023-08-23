36dce866b5322bf21978320c8e58ee86be3162b1
Priyadarashini Chetlur Vasu <priyavasu0502@gmail.com>
2023-08-23 18:13:08 +0200

    Add implementation of matrix to list and reachable nodes

---
+++ matrix_to_listr.py
@@ -0,0 +1,34 @@
+#! usr/bin/env python3
+""" Convert adjacency matrix to an adjacency list representation
+
+    Parameters:
+    -----------
+    adj_mat : (a square 0-1 matrix)
+        Adjacency matrix (n x n) of the graph (of n nodes)
+
+
+    Returns:
+    --------
+     adj_list: `list[list[int]]`
+        The adjacency list of the graph
+
+    """
+def mat_to_list (adj_mat):
+    adj_list = [] # create output list
+    for num_row, row in enumerate(adj_mat):
+        adj_list.append([]) # append empty list inside this list for the next step
+        for num_elem, elem in enumerate(row):
+            if elem == 1:
+                adj_list[num_row].append(num_elem)
+
+    return(adj_list)
+
+
+if __name__ == '__main__':
+    adj_mat = [[0, 1, 0, 1, 0, 0],
+             [0, 0, 1, 0, 0, 0],
+             [1, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 1, 0],
+             [0, 0, 0, 1, 0, 0],
+             [0, 0, 0, 0, 0, 0]]
+    print(mat_to_list(adj_mat))
---
+++ reachable_node.py
@@ -0,0 +1,34 @@
+#! usr/bin/env python3
+
+def reachable(adj_list, start_node):
+    """ Compute the nodes reachable from a start node
+
+    For the above example, reachable([[1, 3], [2], [0], [4], [3], []], 0)) must return {0, 1, 2, 3, 4}
+    and reachable([[1, 3], [2], [0], [4], [3], []], 3) must return {3, 4}
+
+    Parameters:
+    -----------
+    adj_list : the adjacency list of the graph
+    start_node: the index of the start node
+
+    Returns:
+    --------
+    reachable: set(int) the set of nodes reachable from start_node
+
+
+    """
+    return reachable_internal(adj_list, start_node, set(), set())
+
+
+def reachable_internal(adj_list, start_node, reachable_nodes, visited_nodes):
+    reachable_nodes.update(set(adj_list[start_node]))
+    for node in adj_list[start_node]:
+        if node not in visited_nodes:
+            visited_nodes.add(node)
+            reachable_internal(adj_list, node, reachable_nodes, visited_nodes)
+
+    return reachable_nodes
+
+if __name__ == '__main__':
+    print(reachable([[1, 3], [2], [0], [4], [3], []], 0))
+    print(reachable([[1, 3], [2], [0], [4], [3], []], 3))
